5f307c868b226682cd7a8c0a54b53fe40f685e34
Mxrcy0 <122436925+Mxrcy0@users.noreply.github.com>
2023-02-19 11:30:29 +0000

    das esch s file, mit dem mer d elo bechunnt

---
+++ SetELO.php
@@ -0,0 +1,36 @@
+<?php
+// code inspired by stackoverflow (https://stackoverflow.com/questions/18632607/how-to-use-php-to-connect-to-sql-server#:~:text=php%20%24serverName%20%3D%20%22ServerName%22,Connect%20using%20SQL%20Server%20Authentication.)
+// make variables to connect with database
+$myServer = "127.0.0.1.3306";       
+$myUser = "u681823632_jan";
+$myPass = "Jannoahrobin0";
+$myDB = "u681823632_iratasdatabase";
+
+//open connection to database
+$conn = mysqli_connect($myServer, $myUser, $myPass)
+  or die("Couldn't connect to SQL Server on $myServer");
+echo "connected successfully";
+
+//variables, which are fed into database after having been entered by the user
+$data = json_decode(file_get_contents("php://input"));
+$pID = $data->pID;
+$elo = $data->elo;
+
+//check what the players current highscore is and store it on curr_hscore
+$elo_query = "SELECT elo FROM player WHERE pID = $pID";
+$elo_result = $conn->query($elo_query);
+$elo_row = mysqli_fetch_array($elo_result);
+$new_elo = $elo_row['elo'];
+
+//if the players last score is higher than their current highscore, overwrite the highscore in the database
+//feed the data which is stored on the variables into the database
+$sql = "INSERT INTO score (pID, hscore) VALUES ($pID, $score)";
+$result = $conn->query($sql);
+//check if the data was inserted, if not, return an error which describes the problem that occured
+if (!$result) {
+echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+//close connection to database
+mysqli_close($conn)
+?>
